459462a0d20fc56ae629a2bf97d250a09aca2fe1
mini1115 <kmini9611@naver.com>
2023-11-15 16:04:41 +0900

    feat : 일자별 크리스마스 디데이,평일,주말,특별일 할인 금액 계산 기능 추가

--- src/main/java/christmas/service/Calculate.java
+++ src/main/java/christmas/service/Calculate.java
@@ -20,6 +20,34 @@ public class Calculate {
     public int getDishPrice(String dish) {
         return MENU.getMenuPrice(dish).getPrice();
     }
+
+    public int weekdayDiscount(Map<String, String> menu) {
+        int count = 0;
+        for (String key : menu.keySet()) {
+            if (MENUKIND.getDiscountMenu(key).getKind().contains("디저트")) {
+                count += Integer.parseInt(menu.get(key));
+            }
+        }
+        return DISCOUNT_MENU * count;
+    }
+
+    public int weekendDiscount(Map<String, String> menu) {
+        int count = 0;
+        for (String key : menu.keySet()) {
+            if (MENUKIND.getDiscountMenu(key).getKind().contains("메인")) {
+                count += Integer.parseInt(menu.get(key));
+            }
+        }
+        return DISCOUNT_MENU * count;
+    }
+
+    public int christmasDiscount(int day) {
+        int discount = 0;
+        discount = (day - 1) * 100 + 1000;
+        //total+=discount;
+        return discount;
+    }
+
     public MENU hasChampagne(int sum) {
         if (sum >= FREE_CHAMPAGNE_LIMIT) {
             return MENU.CHAMPAGNE;
