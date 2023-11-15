13cc8e83e621292827c686a269fa1f9e354ef5c5
mini1115 <kmini9611@naver.com>
2023-11-15 16:02:36 +0900

    refactor : 기존의 Judgement 클래스 -> Calculate로 클래스명 변경

---
+++ src/main/java/christmas/service/Calculate.java
@@ -0,0 +1,23 @@
+package christmas.service;
+
+import christmas.domain.MENU;
+import christmas.domain.MENUKIND;
+
+import java.util.Map;
+
+public class Calculate {
+    private static final int DISCOUNT_MENU = 2023;
+    private static final int FREE_CHAMPAGNE_LIMIT = 120_000;
+
+    public int totalPrice(Map<String, String> menu) {
+        int sum = 0;
+        for (String key : menu.keySet()) {
+            sum += getDishPrice(key) * Integer.parseInt(menu.get(key));
+        }
+        return sum;
+    }
+
+    public int getDishPrice(String dish) {
+        return MENU.getMenuPrice(dish).getPrice();
+    }
+}
