55d018a336a45d75e8dd0ba30c36e4b7e4baf670
mini1115 <kmini9611@naver.com>
2023-11-15 15:55:54 +0900

    refactor : 혜택 금액대 별 등급 산정 enum 클래스명 변경

--- src/main/java/christmas/domain/BADGE.java
+++ src/main/java/christmas/domain/BADGE.java
@@ -1,21 +1,17 @@
-package christmas;
+package christmas.domain;
 
 import java.util.Arrays;
 
-public enum Badge {
-    //    5천 원 이상: 별
-//    - 1만 원 이상: 트리
-//    - 2만 원 이상: 산타
-    EMPTY(0, 0, "없음"),
+public enum BADGE {
     Star(5000, 10000, "별"),
     Tree(10000, 20000, "트리"),
-    Santa(20000, 1_375_001, "산타");
-
+    Santa(20000, 1_375_001, "산타"),
+    EMPTY(0, 0, "없음");
     private final int min_value;
     private final int max_value;
     private final String grade;
 
-    Badge(int min_value, int max_value, String grade) {
+    BADGE(int min_value, int max_value, String grade) {
         this.min_value = min_value;
         this.max_value = max_value;
         this.grade = grade;
@@ -25,8 +21,8 @@ public enum Badge {
         return this.grade;
     }
 
-    public static Badge getBadge(int min_value, int max_value) {
-        return Arrays.stream(Badge.values()).filter(x -> x.min_value >= min_value && x.max_value < max_value).findAny().orElse(EMPTY);
+    public static BADGE getBadge(int min_value, int max_value) {
+        return Arrays.stream(BADGE.values()).filter(x -> x.min_value <= min_value && x.max_value > max_value).findAny().orElse(EMPTY);
     }
 
     public static String getGrade(int min_value, int max_value) {
