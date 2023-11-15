b84c53bf43bc5e586edd2a99800c295de1b182d3
mini1115 <kmini9611@naver.com>
2023-11-15 16:30:54 +0900

    feat: 메인 함수에서 컨트롤러 실행 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,14 @@
 package christmas;
 
+
+import christmas.controller.OrderController;
+import christmas.view.Input;
+
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OrderController controller = new OrderController();
+        controller.run();
     }
 }
