94df5e1d5e2d90e244f23a990ad616bab2e96083
mini1115 <kmini9611@naver.com>
2023-11-15 16:26:26 +0900

    feat: 전체 할인 금액 출력 및 총 결제금액 출력 기능 추가

--- src/main/java/christmas/view/Output.java
+++ src/main/java/christmas/view/Output.java
@@ -13,6 +13,18 @@ public class Output {
     Figure figure = new Figure();
     Calculate calculate = new Calculate();
     private static int total_discount = 0;
+
+    public void showAll(Map<String, String> menu, int sum, int day) {
+        System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+        printMenu(menu);
+        printBeforeDiscountMoney(sum);
+        int discount = showAllDiscount(menu, sum, day);
+        discount += calculate.hasChampagne(sum).getPrice();
+        showFreeChampagne(sum);
+        showDiscountMoney(discount);
+        printAfterDiscountMoney(sum, discount);
+    }
+
     public void printMenu(Map<String, String> menu) {
         System.out.println("<주문 메뉴>");
         for (String key : menu.keySet()) {
@@ -20,10 +32,30 @@ public class Output {
         }
         System.out.println();
     }
+
     public void printBeforeDiscountMoney(int sum) {
         System.out.println("<할인 전 총주문 금액>");
         System.out.println(df.format(sum) + "원");
+        System.out.println("\n<증정 메뉴>");
+        System.out.println(freeChampaign(sum));
     }
+
+    public int showAllDiscount(Map<String, String> menu, int sum, int day) {
+        System.out.println("\n<혜택 내역>");
+        if (sum < 10000) {
+            System.out.println("없음");
+            return total_discount;
+        }
+        showChristmas(day);
+        showWeekend(day, menu);
+        showWeekday(day, menu);
+        showSpecial(day);
+        if (showFreeChampagne(sum)) {
+            System.out.println("증정 이벤트: -25,000원");
+        }
+        return total_discount;
+    }
+
     public void showChristmas(int day) {
         int discount = 0;
         if (figure.beforeChristmas(day)) {
@@ -56,6 +88,26 @@ public class Output {
             total_discount += 1000;
             System.out.println("특별 할인: -" + df.format(1000) + "원");
         }
+    }
+
+    public boolean showFreeChampagne(int sum) {
+        return (calculate.hasChampagne(sum).getPrice() != 0);
+
+    }
+
+    public void showDiscountMoney(int money) {
+        System.out.println("<총혜택 금액>\n" + df.format(money * -1) + "원");
+    }
+
+    public void printAfterDiscountMoney(int sum, int money) {
+        System.out.println("<할인 후 예상 결제 금액>\n" +
+                df.format(sum - money + calculate.hasChampagne(sum).getPrice()) + "원");
+    }
+    public String freeChampaign(int sum) {
+        if (showFreeChampagne(sum)) {
+            return "샴페인 1개";
+        }
+        return "없음";
     }
 }
 
