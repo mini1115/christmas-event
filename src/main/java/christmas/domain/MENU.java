b758784575e023ac4d88d470bd0d5747d52e8475
mini1115 <kmini9611@naver.com>
2023-11-15 15:57:23 +0900

    refactor : MENU ENUM 클래스 get메소드 추가

---
+++ src/main/java/christmas/domain/MENU.java
@@ -0,0 +1,38 @@
+package christmas.domain;
+
+import java.util.Arrays;
+
+public enum MENU {
+    SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    SALAD("시저샐러드", 8000),
+    STEAK("티본스테이크", 55000),
+    RIB("바비큐립", 54000),
+    SEAFOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+    CAKE("초코케이크", 15000),
+    ICE_CREAM("아이스크림", 5000),
+    COKE("제로콜라", 3000),
+    WINE("레드와인", 60000),
+    CHAMPAGNE("샴페인", 25000),
+    EMPTY("없는메뉴", 0);
+    private final String name;
+    private final int price;
+
+    MENU(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static MENU getMenuPrice(String name) {
+        return Arrays.stream(MENU.values()).filter(x -> x.name.equals(name)).findAny().orElse(EMPTY);
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
