d21a7b24abae4e344f4afc9b500e63437f6591d4
mini1115 <kmini9611@naver.com>
2023-11-15 16:23:00 +0900

    feat: 할인 전 전체 결제 금액 계산 기능 추가

--- src/main/java/christmas/view/Output.java
+++ src/main/java/christmas/view/Output.java
@@ -9,12 +9,17 @@ import java.text.DecimalFormat;
 import java.util.Map;
 
 public class Output {
+    DecimalFormat df = new DecimalFormat("###,###");
     public void printMenu(Map<String, String> menu) {
         System.out.println("<주문 메뉴>");
         for (String key : menu.keySet()) {
             System.out.println(key + " " + menu.get(key) + "개");
         }
         System.out.println();
+    }
+    public void printBeforeDiscountMoney(int sum) {
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(df.format(sum) + "원");
     }
 }
 
