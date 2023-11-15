e48644d187623a3e1bf044a41e61f77ca5bb221c
mini1115 <kmini9611@naver.com>
2023-11-15 16:24:43 +0900

    feat: 일자별 해당하는 할인 이벤트 금액대 출력 기능 추가

--- src/main/java/christmas/view/Output.java
+++ src/main/java/christmas/view/Output.java
@@ -10,6 +10,9 @@ import java.util.Map;
 
 public class Output {
     DecimalFormat df = new DecimalFormat("###,###");
+    Figure figure = new Figure();
+    Calculate calculate = new Calculate();
+    private static int total_discount = 0;
     public void printMenu(Map<String, String> menu) {
         System.out.println("<주문 메뉴>");
         for (String key : menu.keySet()) {
@@ -20,6 +23,39 @@ public class Output {
     public void printBeforeDiscountMoney(int sum) {
         System.out.println("<할인 전 총주문 금액>");
         System.out.println(df.format(sum) + "원");
+    }
+    public void showChristmas(int day) {
+        int discount = 0;
+        if (figure.beforeChristmas(day)) {
+            discount = calculate.christmasDiscount(day);
+            System.out.println("크리스마스 디데이 할인: -" + df.format(discount) + "원");
+        }
+        total_discount += discount;
+    }
+
+    public void showWeekend(int day, Map<String, String> menu) {
+        int discount = 0;
+        if (figure.weekendOrNot(day)) {
+            discount += calculate.weekendDiscount(menu);
+            System.out.println("주말 할인: -" + df.format(discount) + "원");
+        }
+        total_discount += discount;
+    }
+
+    public void showWeekday(int day, Map<String, String> menu) {
+        int discount = 0;
+        if (!figure.weekendOrNot(day)) {
+            discount += calculate.weekdayDiscount(menu);
+            System.out.println("평일 할인: -" + df.format(discount) + "원");
+        }
+        total_discount += discount;
+    }
+
+    public void showSpecial(int day) {
+        if (figure.specialDayOrNot(day)) {
+            total_discount += 1000;
+            System.out.println("특별 할인: -" + df.format(1000) + "원");
+        }
     }
 }
 
